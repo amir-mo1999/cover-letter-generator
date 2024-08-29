5bdd002c351903a3f39b7bc9e4bae3429bcbbc13
amir-mo1999 <amir.mohammad@hotmail.de>
2024-08-29 22:23:59 +0200

    implemented util for generating cover letter and prompts moved prompts to text files

--- api/App/router.py
+++ api/App/router.py
@@ -8,13 +8,21 @@ router = APIRouter()
 
 @router.post("/read-text-from-pdf")
 async def post_resume(pdf_file: UploadFile):
+    # assert that the file is a pdf
     if (
         not pdf_file.filename.endswith(".pdf")
         or not pdf_file.content_type == "application/pdf"
     ):
         raise HTTPException(400, "Not a pdf file")
 
+    # read the content as base64 and extract the text
     content = await pdf_file.read()
     content = base64.b64encode(content).decode("utf-8")
     content = extract_text_from_pdf_base64(content)
     return JSONResponse(content, 200)
+
+
+@router.post("/generate-cover-letter")
+async def generate_cover_letter():
+
+    return "hey"
