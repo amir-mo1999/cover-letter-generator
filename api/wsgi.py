0147c3733cd3010afb292d0abbb48d4c25198760
amir-mo1999 <amir.mohammad@hotmail.de>
2024-08-29 17:03:27 +0200

    set up client and api

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,2 +1,17 @@
+import uvicorn
+from dotenv import load_dotenv
 
-print("hrllo")
+load_dotenv()
+
+# entry point to fastAPI application
+if __name__ == "__main__":
+    uvicorn.run(
+        "App.__init__:app",
+        host="0.0.0.0",
+        port=4000,
+        reload=True,
+        reload_dirs=[
+            "/App",
+        ],
+        forwarded_allow_ips="*",
+    )
